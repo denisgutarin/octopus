d57b152b8a8233d8ae72af98da098f33b94d904b
Denis Gutarin <denis.gutarin@gmail.com>
2021-01-25 01:34:09 +0300

    webpack config

---
+++ webapp/src/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return <h1>Application</h1>;
+};
+export default App;
---
+++ webapp/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',    
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: path.resolve(__dirname, 'node_modules'),
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),    
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true,
+  },
+};
